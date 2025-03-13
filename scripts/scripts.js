dc4b19fe42eeeb14f5d943edc809e6d77dbef709
Maruf220502 <khan2002maruf@gmail.com>
2025-03-13 11:09:48 +0600

    add many part

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,15 +1,30 @@
-
+// button
 function loadCategories(){
     fetch('https://openapi.programming-hero.com/api/phero-tube/categories')
     .then(res => res.json())
     .then(data => displayCategories(data.categories))
 }
-
+// all videos 
 function loadVideos (){
     fetch('https://openapi.programming-hero.com/api/phero-tube/videos')
     .then( res => res.json())
     .then( data => displayVideos(data.videos))
 }
+
+// single button
+ const loadCategoriesVideos = (id) =>{
+    const url = `https://openapi.programming-hero.com/api/phero-tube/category/${id}`;
+    
+    fetch(url)
+    .then(res => res.json())
+    .then(data =>{
+        const clickButton = document.getElementById(`btn-${id}`)
+        clickButton.classList.add("active")
+        displayVideos(data.category)
+    });
+ }
+
+// display dynamic button
   function displayCategories(categories){
     const categoriesContainner = document.getElementById("categories-containner");
     
@@ -20,13 +35,13 @@ function loadVideos (){
             // console.log(cat)
             const categoriesDiv = document.createElement("div")
             categoriesDiv.innerHTML = `
-            <button class="btn hover:bg-[#FF1F3D] hover:text-white">${cat.category}</button>
+            <button id="btn-${cat.category_id}" onclick="loadCategoriesVideos(${cat.category_id})" class="btn hover:bg-[#FF1F3D] hover:text-white">${cat.category}</button>
             `
             categoriesContainner.appendChild(categoriesDiv);
         }
     }
 
-// dynamic button
+// dynamic videos demo
     // // dynamic videos
     // {
     //     "category_id": "1001",
@@ -46,8 +61,26 @@ function loadVideos (){
     //     },
     //     "description": "Dive into the rhythm of 'Shape of You,' a captivating track that blends pop sensibilities with vibrant beats. Created by Olivia Mitchell, this song has already gained 100K views since its release. With its infectious melody and heartfelt lyrics, 'Shape of You' is perfect for fans looking for an uplifting musical experience. Let the music take over as Olivia's vocal prowess and unique style create a memorable listening journey."
     // }
+
+    // 
+
+// display dynamic videos 
+
+
 const displayVideos = (videos) => {
     const videoContainner = document.getElementById("video-contsinner")
+
+    videoContainner.innerHTML = "";
+
+    if(videos.length === 0){
+        videoContainner.innerHTML =`
+        <div class="flex flex-col col-span-full justify-center items-center gap-4 py-5">
+        <img class="w-32" src="assets/Icon.png" alt="">
+        <h1 class="text-3xl font-bold text-center">Oops!! Sorry, There is no<br> content here</h1>
+      </div>
+        `
+        return;
+    }
 
     videos.forEach(video => {
         console.log(video)
@@ -55,20 +88,33 @@ const displayVideos = (videos) => {
         const videoDiv = document.createElement("div")
         videoDiv.innerHTML = `
         <div class="card bg-base-100 shadow-sm">
-  <figure>
-    <img
-      src="${video.thumbnail}"
-      alt="video" />
-  </figure>
-  <div class="card-body">
-    <h2 class="card-title">${video.title}</h2>
-    <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
-  </div>
-</div>
+        <figure class="relative w-full h-36 object-cover">
+          <img 
+            src="${video.thumbnail}"
+            alt="video" />
+            <span class="absolute bottom-2 right-2 p-1 text-white bg-black rounded">3hrs 56 min ago</span>
+        </figure>
+        <div class=" flex gap-5 py-5 px-4">
+          <div class="profile">
+            <div class="avatar">
+            <div class="ring-primary ring-offset-base-100 w-8 rounded-full ring ring-offset-2">
+              <img src="${video.authors[0].profile_picture}" />
+            </div>
+          </div>
+        </div>
+          <div class="intro">
+            <h2 class="text-xl font-semibold mb-1">${video.title}</h2>
+            <div class="flex gap-2 mb-1">
+              <h3 class="text-sm font-semibold text-gray-400">${video.authors[0].profile_name}</h3>
+              <img src="assets/verified.png" alt="">
+            </div>
+            <p class="text-sm font-semibold text-gray-400">${video.others.views} views</p>
+          </div>
+        </div>
+      </div>
         `
         videoContainner.append(videoDiv);
     });
 }
 
 loadCategories()
-loadVideos()
